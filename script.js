6d651e05ad8b941c3a845a7acfa8fc290ddd30e8
lemma09 <asefalemma09@gmail.com>
2026-03-26 16:20:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Button yeroo tuqamu ergaa kana agarsiisa
+function nagaaHullee() {
+    alert("Ergaa keessan nagaan fudhanneerra! Galatoomaa Lammaa quunnamuu keessaniif.");
+}
+<button type="submit" class="btn" onclick="nagaaHullee()">Ergaa Ergi</button>
