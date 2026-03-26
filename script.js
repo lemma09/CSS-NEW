8ac93abeeb09643bd3990e83e87e7d2bffbd4a5c
lemma09 <asefalemma09@gmail.com>
2026-03-26 16:58:13 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -32,3 +32,17 @@ function bifaJijjiiri() {
     // Ergaa xiqqoo akka namni hubatuuf
     console.log("Bifti weebsaayitii jijjiirameera!");
 }
+// 1. Dark Mode Toggle
+function bifaJijjiiri() {
+    document.body.classList.toggle("dark-theme");
+}
+
+// 2. Form Interaction with Name
+function ergaaErgi() {
+    let maqaa = document.querySelector('input[type="text"]').value;
+    if (maqaa === "") {
+        alert("Maaloo, dura maqaa keessan galchaa!");
+    } else {
+        alert("Galatoomaa " + maqaa + "! Ergaa keessan nagaan fudhanneerra.");
+    }
+}
