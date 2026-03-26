c58bc60b862dcaf65b8a4cd0153d7b51d804ff8a
lemma09 <asefalemma09@gmail.com>
2026-03-26 16:46:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function ergaaErgi() {
 function bifaJijjiiri() {
     document.body.classList.toggle("dark-theme");
 }
+window.onload = function() {
+    let maqaa = prompt("Maqaan keessan eenyu?");
+    if (maqaa != null && maqaa != "") {
+        alert("Baga nagaan dhufte " + maqaa + "! Weebsaayitii Lammaa irratti waan gaarii akka argattu abdiin qaba.");
+    }
+}
+function ergaaErgi() {
+    // Input-oota HTML irraa fudhachuu
+    let maqaa = document.querySelector('input[placeholder="Maqaa Keessan"]').value;
+    let email = document.querySelector('input[placeholder="Email Keessan"]').value;
+
+    if (maqaa === "" || email === "") {
+        alert("Maaloo, dura maqaa fi email keessan guutaa!");
+    } else {
+        alert("Galatoomaa " + maqaa + "! Ergaa keessan nagaan fudhanneerra. Gara email keessan: " + email + " irratti deebii isiniif ergina.");
+    }
+}
